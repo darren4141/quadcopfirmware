8a7f5637e874bfe8ad2236861bec17a1627f582c
Darren Liu <darrrenliu@gmail.com>
2025-08-20 15:44:59 -0400

    initial commit

---
+++ src/dmp_firmware.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <stdint.h>
+
+// These come from the MotionApps v2.0 DMP image/config.
+
+extern const uint8_t dmpMemory[];
+extern const unsigned short dmpMemoryLength;
+
+extern const uint8_t dmpConfig[];
+extern const unsigned short dmpConfigLength;
+
+extern const uint8_t dmpUpdates[];
+extern const unsigned short dmpUpdatesLength;
