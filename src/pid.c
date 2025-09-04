f902fd7bf9a3cc8e874366c3f3127bd23c533cf7
Darren Liu <darrrenliu@gmail.com>
2025-09-03 22:31:45 -0400

    added deadband

--- src/pid.c
+++ src/pid.c
@@ -10,6 +10,11 @@
 void PIDinitialize(pid_controller controller){
     controller.prev_time_us = esp_timer_get_time();
     controller.eI = 0;
+
+    //default settings, no upper bound, no max integral, no deadband
+    controller.upperBound = 0;
+    controller.maxI = 0;
+    controller.eDeadband = 0;
 }
 
 void setTarget(pid_controller controller, float new_target){
@@ -24,12 +29,23 @@ void setEIBound(pid_controller controller, float new_maxI){
     controller.maxI = new_maxI;
 }
 
+void setEDeadband(pid_controller controller, float new_eDeadband){
+    controller.eDeadband = new_eDeadband;
+}
+
 float PIDCalculate(pid_controller controller, float current){
     float dT = (esp_timer_get_time() - controller.prev_time_us) / 1e6;
     float eP = controller.target - current;
+
+    //deadband check
+    if(ABS(eP) < controller.eDeadband){
+        return 0;
+    }
+
     float eI = controller.eI + (eP * dT);
     float eD = (eP - controller.errorPrevious) / dT;
 
+    //check and obey integral bound
     if((controller.maxI != 0) && ABS(eI) > controller.maxI){
         if(eI > 0){
             eI = controller.maxI;
@@ -38,8 +54,10 @@ float PIDCalculate(pid_controller controller, float current){
         }
     }
 
+    //calculate PID
     float res = (controller.kP * eP) + (controller.kI * eI) + (controller.kD * eD);
 
+    //check and obey upper bound
     if((controller.upperBound != 0) && ABS(res) > controller.upperBound){
         if(res > 0){
             res = controller.upperBound;
--- src/pid.h
+++ src/pid.h
@@ -11,10 +11,11 @@ typedef struct{
     float kI;
     float kD;
     
-    float maxI;
-    float upperBound;
+    float maxI;         //max value of integral term eI, default value of 0 will result in no bound
+    float upperBound;   //max value of pid calculation, default value of 0 will result in no bound
 
-    float eI;
+    float eI; //integral term
+    float eDeadband; //below a certain value of eP the pid calculation will return 0
 
     float target;
     float errorPrevious;
@@ -29,5 +30,7 @@ void setTarget(pid_controller controller, float new_target);
 void setUpperBound(pid_controller controller, float new_bound);
 
 void setEIBound(pid_controller controller, float new_maxI);
+
+void setEDeadband(pid_controller controller, float new_eDeadband);
 
 float PIDCalculate(pid_controller controller, float current);
